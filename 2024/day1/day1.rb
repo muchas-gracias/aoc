c711d0046299b22d7d315a4263f0db9f1e1cc57b
Gregory Nelson <gregory.nelson3.mil@mail.mil>
2024-12-13 15:19:48 -0600

    loaded files

---
+++ 2024/day1/day1.rb
@@ -0,0 +1,51 @@
+
+def part_2(left, right)
+    total = 0
+
+    left.each do |item|
+        k = item.to_i
+        total += k * right.count(k)
+    end
+
+    puts total
+
+    return
+end
+
+def part_1(left, right)
+    total = 0
+
+    left.each_with_index do |value, i|
+        total += (value - right[i]).abs
+    end
+    puts total
+    return total
+end
+
+def append_lists(lines)
+    left = []
+    right = []
+
+    lines.each do |line|
+        line = line.strip.split
+        left << line[0].to_i
+        right << line[1].to_i
+    end
+
+    return left, right
+end
+
+def main
+    lines = File.readlines('input.txt')
+
+    left, right = append_lists(lines)
+    left  = left.sort
+    right = right.sort
+
+    total = part_1(left, right)
+    part_2(left, right)
+
+end
+
+
+main
